8f7b5decfe69ffe9ad70d7d08e93e2d0410fa3ab
thusharashenoi <thushara.shenoi@gmail.com>
2023-08-15 18:07:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Add smooth scrolling to all links with hashes
+    document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+        anchor.addEventListener("click", function(e) {
+            e.preventDefault();
+            
+            // Get the target section's ID from the href attribute
+            var target = document.querySelector(this.getAttribute("href"));
+            
+            // Scroll smoothly to the target section
+            target.scrollIntoView({
+                behavior: "smooth",
+                block: "start", // Scroll to the top of the target
+                inline: "nearest", // Align to the nearest edge
+                duration: 54000000
+            });
+        });
+    });
+});
